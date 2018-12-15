97171fcd165e9f543ae1fd7c13b370aad0435646
azure1016 <908541982@qq.com>
2018-12-15 10:51:31 -0500

    Update ValidParentheses.cpp

--- leetcode_Cpp/ValidParentheses.cpp
+++ leetcode_Cpp/ValidParentheses.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    bool isValid(string s) {
+    bool isValid (string s) {
         stack<char> symbols;
-        if(s.length()==1)
+        if(s.length () == 1)
             return false;
-        else if(s.length()==0)
+        else if(s.length () == 0)
             return true;
         else {
-        symbols.push(s[0]);
-        for(int i=1;i<s.length();i++){
-            if(symbols.size()==0)
-                symbols.push(s[i]);
-            else if(symbols.top()==s[i]-1 ||symbols.top()==s[i]-2)
-               symbols.pop();
-            else symbols.push(s[i]);
+        symbols.push (s[0]);
+        for(int i = 1; i < s.length (); i++){
+            if(symbols.size () ==0 )
+                symbols.push (s[i]);
+            else if(symbols.top () == s[i] - 1 || symbols.top () == s[i] - 2)
+               symbols.pop ();
+            else symbols.push (s[i]);
         }
-        if(symbols.size()==0)
+        if(symbols.size () == 0)
             return true;
         else return false;
         }
